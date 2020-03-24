ca93583ddeef3effcb17a9e18e2e13265d1f1f4c
tasnidmahin <tasnidmahin@gmail.com>
2020-03-24 18:11:54 +0600

    signup model and register controller added

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaidEasy.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/SignupModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaidEasy.Models
+{
+    public class SignupModel
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string PhoneNo { get; set; }
+        public string PresentAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
