56a11e27da1cb15679131c3b886bfa9cca2235a9
tasnidmahin <tasnidmahin@gmail.com>
2020-03-24 10:11:39 +0600

    login & signup page design added

---
+++ Scripts/JS/index.js
@@ -0,0 +1,21 @@
+﻿/*< !--The core Firebase JS SDK is always required and must be listed first-- >
+    <script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js"></script>
+
+    <!--TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-analytics.js"></script>*/
+
+// Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBD4w9BqnVt7_nez6N910TMT9vNkDLwzac",
+    authDomain: "maideasy-8db0a.firebaseapp.com",
+    databaseURL: "https://maideasy-8db0a.firebaseio.com",
+    projectId: "maideasy-8db0a",
+    storageBucket: "maideasy-8db0a.appspot.com",
+    messagingSenderId: "317604517096",
+    appId: "1:317604517096:web:abf8efcf60803c5beba55d",
+    measurementId: "G-JENXVY6Q7J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
