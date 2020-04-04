4130292c247c4e869d537949e47ae683d8221fbe
tasnidmahin <tasnidmahin@gmail.com>
2020-04-04 08:31:05 +0600

    service model and controller added

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaidEasy.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+    }
+}
