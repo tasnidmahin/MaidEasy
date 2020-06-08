cae9770f85af0814c64130f40e66036c1d31d38f
tahmidtaqi <47303121+tahmidtaqi@users.noreply.github.com>
2020-06-08 20:48:12 +0600

    Admin home page and login page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaidEasy.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Admin_home()
+        {
+            return View();
+        }
+        public ActionResult Admin_login()
+        {
+            return View();
+        }
+    }
+}
