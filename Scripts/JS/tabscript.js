f2174edd1b2a5e9289437cf850674f14df10001d
tahmidtaqi <taqitahmid97@gmail.com>
2020-08-12 21:16:19 +0600

    Tab problem fixed and logo added

--- Scripts/JS/tabscript.js
+++ Scripts/JS/tabscript.js
@@ -1,4 +1,5 @@
-﻿const tabs = document.querySelectorAll('[data-tab-target]')
+﻿/*
+const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
 tabs.forEach(tab => {
@@ -16,4 +17,15 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 
-})
+}) */
+
+$(document).ready(function () {
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+        localStorage.setItem('activeTab', $(e.target).attr('href'));
+    });
+    var activeTab = localStorage.getItem('activeTab');
+    if (activeTab) {
+        $('#myTab a[href="' + activeTab + '"]').tab('show');
+    }
+    console.log('here');
+});
