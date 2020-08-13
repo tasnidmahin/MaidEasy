334d0f09a27dc53a16b2a74ff96ec43710b01a54
tasnidmahin <tasnidmahin@gmail.com>
2020-08-13 19:36:33 +0600

    Edit worker model added

---
+++ Models/EditWorkerModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaidEasy.Models
+{
+    public class EditWorkerModel
+    {
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public string Phone { get; set; }
+        public string PresentAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public string gender { get; set; }
+        public string type { get; set; }
+        public string area { get; set; }
+        public string image { get; set; }
+
+        public EditWorkerModel(int id)
+        {
+            string sql = "SELECT Name,fatherName,mobile,PresentAddress,PermanentAddress,gender,type,Area,image from worker where WorkerId = '" + id + "'";
+
+            DBHelper db = DBHelper.getDB();
+            var table = db.getData(sql);
+            table.Read();
+
+            Name                = table.GetString(0);
+            FatherName          = table.GetString(1);
+            Phone               = table.GetString(2);
+            PresentAddress      = table.GetString(3);
+            PermanentAddress    = table.GetString(4);
+            gender              = table.GetString(5);
+            type                = table.GetString(6);
+            area                = table.GetString(7);
+            image               = table.GetString(8);
+        }
+    }
+}
--- Models/MaidProfileModel.cs
+++ Models/MaidProfileModel.cs
@@ -8,5 +8,6 @@ namespace MaidEasy.Models
     //
     public class MaidProfileModel
     {
+
     }
 }
