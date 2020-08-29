b7d98e0004ac673bc179efe388d5fc95b0fa40ac
tahmidtaqi <taqitahmid97@gmail.com>
2020-08-29 07:30:09 +0600

    add worker and userlist issues solved

---
+++ Scripts/JS/Addworker.js
@@ -0,0 +1,17 @@
+﻿
+
+$(document).ready(function () {
+    $('#submitbtn').click(function () {
+        checked = $("input[type=checkbox]:checked").length;
+
+        if (!checked) {
+            // alert("You must check at least one checkbox.");
+            document.getElementById("invalid-message").style.display = "block";
+            return false;
+        }
+        if (checked) {
+            document.getElementById("invalid-message").style.display = "hidden";
+        }
+
+    });
+});
