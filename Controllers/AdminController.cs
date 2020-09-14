552a8e3c6953596bc38fca7ecb04f66ba61a8392
Swaad <47058115+Swaad@users.noreply.github.com>
2020-09-14 22:12:44 +0600

    WorkList fully done!

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using MaidEasy.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace MaidEasy.Controllers
 {
     public class AdminController : Controller
     {
+        CustomDbContext Db = new CustomDbContext();
         // GET: Admin
         public ActionResult Index()
         {
@@ -22,8 +24,45 @@ namespace MaidEasy.Controllers
         }
         public ActionResult Worklist()
         {
-            return View();
+            return View(Db.work.ToList());
         }
+
+        [HttpPost]
+        public ActionResult WorklistEdit([Bind(Include = "")] work work)
+        {
+            if (ModelState.IsValid)
+            {
+                Db.Entry(work).State = EntityState.Modified;
+                Db.SaveChanges();
+            }
+            return RedirectToAction("Worklist");
+        }
+
+        [HttpPost]
+        public ActionResult WorklistCreate([Bind(Include = "")] work work)
+        {
+
+            // TODO: Add insert logic here
+            if (ModelState.IsValid)
+            {
+                Db.work.Add(work);
+                Db.SaveChanges();
+
+            }
+
+            return RedirectToAction("Worklist");
+        }
+
+        [HttpPost, ActionName("WorklistDelete")]
+        public ActionResult WorklistDeleteConfirm(int id)
+        {
+            work work = Db.work.Find(id);
+            Db.work.Remove(work);
+            Db.SaveChanges();
+            Session["ADMIN_WORKLIST_DELETE"] = "Admin Worklist Delete";
+            return RedirectToAction("Worklist");
+        }
+
         public ActionResult Edit_work()
         {
             return View();
